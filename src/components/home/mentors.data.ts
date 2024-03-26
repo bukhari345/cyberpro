d80ae6203dd3534562e28e397429fb83c9434454
hussainali6 <alisyedhussain22@gmail.com>
2024-03-27 03:39:12 +0500

    chnages

--- src/components/home/mentors.data.ts
+++ src/components/home/mentors.data.ts
@@ -37,16 +37,4 @@ export const data: Array<Mentor> = [
       logo: '/images/companies/airbnb.png',
     },
   },
-  {
-    id: 4,
-    photo: '/images/mentors/men4.jpg',
-    name: 'Rizki Known',
-    category: 'Fullstack Development',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    company: {
-      name: 'Microsoft',
-      logo: '/images/companies/microsoft.png',
-    },
-  },
 ]
